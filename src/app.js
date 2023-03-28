e64dd42f450073647ae14bc411f9e1f4c9417520
Electro <75930721+Electro-Magnet45@users.noreply.github.com>
2023-03-28 15:51:43 +0530

    initial

--- src/app.js
+++ src/app.js
@@ -1,34 +1,34 @@
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-const app = express()
-const httpolyglot = require('httpolyglot')
-const https = require('https')
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const httpolyglot = require("httpolyglot");
+const https = require("https");
 
 //////// CONFIGURATION ///////////
 
 // insert your own ssl certificate and keys
 const options = {
-    key: fs.readFileSync(path.join(__dirname,'..','ssl','key.pem'), 'utf-8'),
-    cert: fs.readFileSync(path.join(__dirname,'..','ssl','cert.pem'), 'utf-8')
-}
+  key: fs.readFileSync(
+    path.join(__dirname, "..", "ssl", "localhost.key"),
+    "utf-8"
+  ),
+  cert: fs.readFileSync(
+    path.join(__dirname, "..", "ssl", "localhost.crt"),
+    "utf-8"
+  ),
+};
 
-const port = process.env.PORT || 3012
+const port = process.env.PORT || 3012;
 
 ////////////////////////////
 
-require('./routes')(app)
+require("./routes")(app);
 
-const httpsServer = httpolyglot.createServer(options, app)
-const io = require('socket.io')(httpsServer)
-require('./socketController')(io)
-
+const httpsServer = httpolyglot.createServer(options, app);
+const io = require("socket.io")(httpsServer);
+require("./socketController")(io);
 
 httpsServer.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
-
-
-
-
-
+  console.log(`listening on port ${port}`);
+});
